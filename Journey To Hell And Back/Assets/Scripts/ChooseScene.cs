51adbe0d3f5bee08d233d99355209dc70855db63
Joe Davis <joe--davis@hotmail.com>
2019-02-05 21:07:20 +0000

    v1.2
    + Added scene management
    + Basic UI

---
+++ Journey To Hell And Back/Assets/Scripts/ChooseScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseScene : MonoBehaviour {
+
+	string gameScene = "Game";
+	string rulesScene = "Rules";
+	string creditsScene = "Credits";
+	string mainMenuScene = "Main Menu";
+
+	// Use this for initialization
+	void Start () {
+		 
+		
+	}
+	
+	public void startGameScene(){
+		SceneManager.LoadScene(this.gameScene);
+	}
+
+	public void startRulesScene(){
+		SceneManager.LoadScene(this.rulesScene);
+	}
+
+	public void startCreditsScene(){
+		SceneManager.LoadScene(this.creditsScene);
+	}
+
+	public void startMainMenuScene(){
+		SceneManager.LoadScene(this.mainMenuScene);
+	}
+
+}
