1885f82468eb6772daf68b68920d3ce6a2707e04
Joe Davis <joe--davis@hotmail.com>
2019-02-08 22:22:05 +0000

    v1.5
    + Gameplay is so, much, better!!
    + One-way platforms.

--- Journey To Hell And Back/Assets/CoreCode/HealthItem.cs
+++ Journey To Hell And Back/Assets/CoreCode/HealthItem.cs
@@ -2,15 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health_Item_Script : MonoBehaviour {
+public class HealthItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed;
+	public float initialSpeed;
+	public float jumpForce;
+	public float jumpTime;
+	public float jumpTimeCounter;
+
+	private float moveLeftAndRight;
+
+	public bool grounded;
+	public LayerMask whatIsGround;
+	public bool stoppedJumping;
+	public bool goingRight;
+	public bool goingLeft;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	private SpriteRenderer mySpriteRenderer;
+
+	public static bool jump;
+	public static bool left;
+	public static bool right;
+
+	private Rigidbody2D rb2d;
+
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+		jumpTimeCounter = jumpTime;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update(){
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (grounded) {
+			jumpTimeCounter = jumpTime;
+		}
+
+		if (!grounded) {
+			speed = (initialSpeed * 0.9f);
+		} else {
+			speed = initialSpeed;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		// This is designed for keyboard input at the moment.
+		moveLeftAndRight = Input.GetAxisRaw ("Horizontal");
+		rb2d.velocity = new Vector2 (moveLeftAndRight * speed, rb2d.velocity.y);
+
+		jump = Input.GetKey (KeyCode.Space);
+		left = Input.GetKey (KeyCode.LeftArrow);
+		right = Input.GetKey (KeyCode.RightArrow);
+
+		// If I press down the key...
+		if (jump == true) {
+			if (grounded) {
+				//rb2d.velocity = new Vector2 (moveLeftAndRight, jumpForce);
+				rb2d.AddForce (transform.up * jumpForce);
+				//rb2d.velocity = (new Vector2 (0, jumpForce));
+				stoppedJumping = false;
+				groundCheckRadius = 0f;
+			}
+		}
+
+		// If I keep holding the key...
+		if ((jump == true) && !stoppedJumping){
+			if (jumpTimeCounter > 0) {
+				//rb2d.velocity = new Vector2 (moveLeftAndRight, jumpForce);
+				rb2d.velocity = (new Vector2 (0, jumpForce));
+				jumpTimeCounter -= Time.deltaTime;
+			}		
+		}
+
+		// If I stop holding the key...
+		if (jump != true) {
+			jumpTimeCounter = 0;
+			stoppedJumping = true;
+			groundCheckRadius = 0.3f;
+		}
+
+		FaceDirection (left, right);
+
+	}
+
+	void FaceDirection(bool left, bool right){
+		if (left == true) {
+			goingLeft = true;
+			mySpriteRenderer.flipX = false;
+			goingRight = false;
+		}
+
+		if (right == true) {
+			goingRight = true;
+			goingLeft = false;
+			mySpriteRenderer.flipX = true;
+		}
 	}
 }
--- Journey To Hell And Back/Assets/CoreCode/MainCharacterInputControl.cs
+++ Journey To Hell And Back/Assets/CoreCode/MainCharacterInputControl.cs
@@ -8,7 +8,10 @@ public class MainCharacterInputControl : MonoBehaviour {
 	public float jumpForce;
 	public float jumpTime;
 	public float jumpTimeCounter;
+	public float groundDist;
+	public int initialExtraJumps;
 
+	private int extraJumps;
 	private float moveLeftAndRight;
 
 	public bool grounded;
@@ -41,61 +44,84 @@ public class MainCharacterInputControl : MonoBehaviour {
 		if (grounded) {
 			jumpTimeCounter = jumpTime;
 		}
-
-
 	}
 
 	void FixedUpdate ()
 	{
 		// This is designed for keyboard input at the moment.
+		jump = Input.GetKeyDown (KeyCode.UpArrow);
+		left = Input.GetKey (KeyCode.A);
+		right = Input.GetKey (KeyCode.D);
 		moveLeftAndRight = Input.GetAxisRaw ("Horizontal");
-		Vector2 moveHorizontal = new Vector2 (moveLeftAndRight, 0);
-		rb2d.AddForce (moveHorizontal * speed);
-		rb2d.velocity = new Vector2 (moveLeftAndRight, 0);
-
-		jump = Input.GetKey ("w");
-		left = Input.GetKey ("a");
-		right = Input.GetKey ("d");
+		rb2d.velocity = new Vector2 (moveLeftAndRight * speed, rb2d.velocity.y);
+		checkGround();
 
 		// If I press down the key...
-		if (jump == true) {
+		if (jump) {
 			if (grounded) {
-				rb2d.velocity = new Vector2 (moveLeftAndRight, jumpForce);
+				rb2d.velocity = Vector2.up * jumpForce;
 				stoppedJumping = false;
-				groundCheckRadius = 0f;
 			}
 		}
 
 		// If I keep holding the key...
-		if ((jump == true) && !stoppedJumping){
+		if ((jump) && !stoppedJumping){
 			if (jumpTimeCounter > 0) {
-				rb2d.velocity = new Vector2 (moveLeftAndRight, jumpForce);
+				//rb2d.velocity = new Vector2 (moveLeftAndRight, jumpForce);
+				rb2d.velocity = (new Vector2 (0, jumpForce));
 				jumpTimeCounter -= Time.deltaTime;
 			}		
 		}
 
 		// If I stop holding the key...
-		if (jump != true) {
+		if (!jump) {
 			jumpTimeCounter = 0;
 			stoppedJumping = true;
-			groundCheckRadius = 0.3f;
 		}
-
+			
 		FaceDirection (left, right);
-
+		DoubleJump ();
 	}
 
 	void FaceDirection(bool left, bool right){
-		if (left == true) {
+		if (left) {
 			goingLeft = true;
 			mySpriteRenderer.flipX = false;
 			goingRight = false;
 		}
 
-		if (right == true) {
+		if (right) {
 			goingRight = true;
 			goingLeft = false;
 			mySpriteRenderer.flipX = true;
+		}
+	}
+
+	void DoubleJump(){
+
+		if (grounded) {
+			extraJumps = initialExtraJumps;
+		}
+
+		if (jump && extraJumps > 0) {
+			rb2d.velocity = Vector2.up * jumpForce;
+			extraJumps--;
+		}else if (jump && extraJumps == 0 && grounded){
+			rb2d.velocity = Vector2.up * jumpForce;
+		}
+	}
+
+	//Shoot a ray out of the bottom to see if the distance is within a certain tolerance.
+	void checkGround(){
+		RaycastHit hit;
+
+		if(Physics.Raycast (transform.position, Vector3.down,out hit, Mathf.Infinity)){
+			if(hit.distance < groundDist){
+				grounded = true;
+			}
+			else{
+				grounded = false;
+			}
 		}
 	}
 }
