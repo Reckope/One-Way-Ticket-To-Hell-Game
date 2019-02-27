3916b5a6f724dafa9b270adc27fef2f290e2314f
Joe Davis <joe--davis@hotmail.com>
2019-02-27 22:29:08 +0000

    v2.1
    - Added a timer with the format "00:00".
    - Repositioned the 'Score' and 'Time' UI.

---
+++ Journey To Hell And Back/Assets/CoreCode/TotalTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalTime : MonoBehaviour{
+	public Text totalTimeText;
+	public static float totalTime;
+
+	void Start(){
+
+	}
+
+	void Update(){
+		StartTimer ();
+		DisplayTime ();
+	}
+
+	void StartTimer(){
+		totalTime += Time.deltaTime;
+	}
+
+	void DisplayTime(){
+		int minutes = Mathf.FloorToInt(totalTime / 60F);
+		int seconds = Mathf.FloorToInt(totalTime - minutes * 60);
+		string niceTime = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+		totalTimeText.text = niceTime;
+	}
+
+	void DisplayFinalTime(){
+		
+	}
+}
